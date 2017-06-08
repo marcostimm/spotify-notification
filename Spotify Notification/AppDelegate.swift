c09a886246fbaa41e45bd2a3b33dcc19eb213907
Marcos Timm <timm@marcos.im>
2017-06-08 00:02:58 -0300

    Initial Commit

---
+++ Spotify Notification/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Spotify Notification
+//
+//  Created by Marcos Timm on 08/06/17.
+//  Copyright © 2017 Timm. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
